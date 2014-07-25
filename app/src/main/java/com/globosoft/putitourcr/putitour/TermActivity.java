01bb335a8a5019e7a660fddfa7f13c59b7dd71c1
Fabián Araya <josefabian2588@gmail.com>
2014-07-25 16:14:56 -0600

    3 subida
    3 subida terminos

---
+++ app/src/main/java/com/globosoft/putitourcr/putitour/TermActivity.java
@@ -0,0 +1,16 @@
+package com.globosoft.putitourcr.putitour;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Desarrollo Trabajo on 25/07/2014.
+ */
+public class TermActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terminos);
+    }
+}
