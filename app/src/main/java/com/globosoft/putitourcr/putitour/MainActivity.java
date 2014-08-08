5859166fa4551b725605cd1cc36e215897cbb505
Fabián Araya <josefabian2588@gmail.com>
2014-08-07 18:39:15 -0600

    07*/08/2014

--- app/src/main/java/com/globosoft/putitourcr/putitour/MainActivity.java
+++ app/src/main/java/com/globosoft/putitourcr/putitour/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class MainActivity extends Activity {
@@ -11,7 +12,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+
         setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.barratitulo);
     }
 
 
--- app/src/main/java/com/globosoft/putitourcr/putitour/TermActivity.java
+++ app/src/main/java/com/globosoft/putitourcr/putitour/TermActivity.java
@@ -1,7 +1,10 @@
 package com.globosoft.putitourcr.putitour;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Desarrollo Trabajo on 25/07/2014.
@@ -11,6 +14,37 @@ public class TermActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.terminos);
+
+        Button btnIngresar = (Button) findViewById(R.id.btnIngresar);
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent Main = new Intent(TermActivity.this,MainActivity.class);
+                startActivity(Main);
+            }
+        });
+
+
+        Button btnSalir = (Button) findViewById(R.id.btnSalir);
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
+
+
     }
+
 }
+
+
+
